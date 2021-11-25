707110d9d8e79c80ee5eded4d5c40889d0508683
vishnukumar25 <88477015+vishnukumar25@users.noreply.github.com>
2021-11-25 09:07:16 +0530

    Update header.c

--- 3_Implementation/header/header.c
+++ 3_Implementation/header/header.c
@@ -5,61 +5,11 @@
 
 struct Person
 {
-   
 };
-
 void appendData();
 void showAllData();
 void findData();
 void deleteData();
 void displayMenu();
 
-int main()
-{
-   
-}
 
-
-void appendData()
-{
-      
-}
-
-void showAllData()
-{
-      
-}
-
-void findData()
-{
-     
-     
-}
-void deleteData()
-{
-	 
-}
-void displayMenu()
-{   
-   
-
-    switch(choice)
-    {
-         case '1' : //call append record
-            appendData();
-            goto mainMenu;
-         case '2' : //call find record
-            findData();
-            goto mainMenu;
-         case '3' : //Read all record
-            showAllData();
-            goto mainMenu;
-         case '4' :
-         	deleteData();
-         	goto mainMenu;
-         case '5':
-            break;
-         default:
-            goto mainMenu;
-    }
-}
